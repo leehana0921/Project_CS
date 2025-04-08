8d6db9356215f9e9add95938b3b7ba8f2022016c
이하나 <45592710+leehana0921@users.noreply.github.com>
2025-04-08 16:02:02 +0900

    [feat] Creat_Player
    기본적인 플레이어의 기능을 생성

---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject_CS
+{
+    public class Player
+    {
+        public int speed = 5;
+        public int power = 10;
+        public int observation = 2;
+        public int gold = 10000;
+
+        public Player()
+        {
+
+        }
+    }
+}
