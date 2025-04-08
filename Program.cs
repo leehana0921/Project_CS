ad4a5de7428cc2930aa82f8cd160eef5c3adb803
이하나 <45592710+leehana0921@users.noreply.github.com>
2025-04-09 01:21:38 +0900

    [feat] 메인 기본 설정

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NewProject_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 게임 시작을 위해 우선 설정해 두기
+            Game.Start();
+            Game.Run();
+            Game.End();
+        }
+    }
+}
