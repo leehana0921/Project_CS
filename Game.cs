5b3e0d13328b2e92c6686008a0b1d6989f8355e5
이하나 <45592710+leehana0921@users.noreply.github.com>
2025-04-08 17:03:28 +0900

    [feat] Creat_Game
    게임이 돌아가게 하기 위해 스태틱으로 게임 클래스 선언

---
+++ Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject_CS
+{
+    public static class Game
+    {
+        private static bool gameOver;
+
+        private static Dictionary<string, Scene> sceneDic;
+        private static Scene curScene;
+
+        public static void Start()
+        {
+            // 게임에 있는 모든 씬들을 보관하고 빠르게 찾아줄 용도로 쓸 자료구조
+            sceneDic = new Dictionary<string, Scene>();
+            sceneDic.Add("Title", new TitleScene());
+
+            curScene = sceneDic["Title"];
+        }
+
+        public static void End()
+        {
+
+        }
+
+        public static void Run()
+        {
+            while (gameOver == false)
+            {
+                curScene.Render();
+                curScene.Choice();
+                curScene.Input();
+                curScene.Result();
+                curScene.Wait();
+                curScene.Next();
+            }
+        }
+
+    }
+}
