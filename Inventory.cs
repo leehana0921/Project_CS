a00eab57dff6807bc1ad54082d894793e2373125
이하나 <45592710+leehana0921@users.noreply.github.com>
2025-04-09 15:18:43 +0900

    [feat] 인벤토리 구현

---
+++ Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject_CS
+{
+    public class Inventory
+    {
+        private List<string> items;
+
+        public Inventory()
+        {
+            items = new List<string>();
+        }
+
+        public void Add(string item)
+        {
+            items.Add(item);
+        }
+
+        public void Remove(string item)
+        {
+            items.Remove(item);
+        }
+    }
+}
